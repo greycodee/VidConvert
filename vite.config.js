d16291501a96176117cd36ec840cbdb6d763047e
greycode <zhengminghui99@gmail.com>
2023-06-25 19:28:49 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VidConvert/',
   plugins: [
     react(),
     crossOriginIsolation()
