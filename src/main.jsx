5556187e12599340af2be8748ffaabfbfa9c466e
greycode <zhengminghui99@gmail.com>
2023-06-25 20:06:14 +0800

    add enable-threads.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Helmet } from 'react-helmet';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Helmet>
-        <meta http-equiv="Cross-Origin-Embedder-Policy" content="require-corp" />
-        <meta http-equiv="Cross-Origin-Opener-Policy" content="same-origin" />
-      </Helmet>
     <App />
   </React.StrictMode>,
 )
