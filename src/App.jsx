98f15fee4ed7bb6fac39c10472c8c9889cc0189f
greycodee <zhengminghui99@gmail.com>
2023-07-03 17:58:45 +0800

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -54,23 +54,7 @@ function App() {
       opacity-70 text-white
       flex justify-center items-center`}
       >
-
-        <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          />
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM12 20.735a8 8 0 01-8-8H0c0 4.411 3.589 8 8 8v-2.265zm3-5.291a7.962 7.962 0 01-2 5.291v2.265a8 8 0 008-8h-2zM20 12a8 8 0 01-8 8v4c4.411 0 8-3.589 8-8h-4zm-2-5.291a7.962 7.962 0 012-5.291v-2.265a8 8 0 00-8 8h2z"
-          />
-        </svg>
-        <p className="text-white">Loading ffmpeg.wasm...</p>
+        <p className="text-white">Your browser not support</p>
       </div>
       <Sidebar
         className="hidden sm:block"
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,32 @@
 import { Link } from "react-router-dom";
 
+import m3u8Logo from "../assets/m3u8.svg";
+
 function Sidebar({className, isOpen, toggleSidebar, itemData }) {
   return (
-    <div className={`h-full ${isOpen ? "w-48":"w-20"} bg-sky-200 transition-all duration-500 ease-in-out
+    <div className={`h-full ${isOpen ? "w-48":"w-11"} bg-slate-800 transition-all duration-500 ease-in-out
     ${className} `}
-    // style={{ display: isOpen ? "block" : "none" }}
     >
-      {/* <div
-        className="fixed top-0 left-0 h-full w-full bg-black opacity-50 z-20"
-        onClick={toggleSidebar}
-        style={{ display: isOpen ? "block" : "none" }}
-      ></div> */}
       <div>
         <button onClick={
           toggleSidebar
         }>close</button>
-        <nav className="mt-8">
+        <nav className="mt-8 ring-1 ring-red-500">
           <ul>
             {itemData.map((item) => (
               <li
                 key={item.key}
                 onClick={toggleSidebar}
-                className="px-4 py-2 text-gray-700 hover:bg-gray-100 hover:bg-sky-300 hover:text-white cursor-pointer"
+                className="ring-1 px-2 py-2 text-gray-700 hover:bg-gray-100 hover:bg-sky-300 hover:text-white cursor-pointer"
               >
                 <Link key={item.key} to={item.link}>
-                  {item.title}
+                  <div className="flex flex-row items-center  ring-1 ring-green-800">
+                   <img src={m3u8Logo} className="w-6 h-6"/>
+                    <span 
+                    className={`${isOpen ? "visible":"invisible"} ml-2 text-white text-sm whitespace-nowrap transition-all duration-200 ease-out`}>
+                      {item.title}
+                      </span>
+                  </div>
                 </Link>
               </li>
             ))}
